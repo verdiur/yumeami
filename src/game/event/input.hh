89ea73230d4240932db1ba5cefbf509bd113e9e9
🌿 <116896120+verdiur@users.noreply.github.com>
2025-07-14 10:44:26 +0000

    style: cleanup

--- src/game/event/input.hh
+++ src/game/event/input.hh
@@ -10,47 +10,6 @@ namespace yumeami {
 
   void dispatch_input_events(entt::dispatcher& dispatcher);
 
-  namespace comp {
-
-    // /**
-    //  * @class KeyboardKeyQueue
-    //  * @brief Queue for KeyboardKey events.
-    //  * To be used as a component.
-    //  * WARNING: deprecated, currently working on a more robust input system
-    //  *
-    //  */
-    // class KeyboardKeyQueue
-    // {
-    // public:
-    //   const std::queue<KeyboardKey>& get_queue() const;
-    //
-    //   /**
-    //    * @brief Receive a KeyboardKeyEvent and queue it.
-    //    *
-    //    * @param event Received event
-    //    */
-    //   void receive(const KeyboardKeyEvent& event);
-    //
-    //   /**
-    //    * @brief Consume a key from the queue.
-    //    * The key will be popped out, and then returned.
-    //    * The queue must be non-empty.
-    //    *
-    //    * @return Consumed key
-    //    */
-    //   KeyboardKey consume();
-    //
-    //   /**
-    //    * @brief Clear queue.
-    //    */
-    //   void clear();
-    //
-    // private:
-    //   std::queue<KeyboardKey> queue = {};
-    // };
-
-  } // namespace comp
-
   namespace sys {
 
     /**
