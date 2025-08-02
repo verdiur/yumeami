d73b6bd3dd6b112c8e51d67e4f33ba59ab93093c
🌿 <116896120+verdiur@users.noreply.github.com>
2025-08-02 21:43:06 +0000

    feat: add units.hh and world.hh

---
+++ src/game/world.hh
@@ -0,0 +1,9 @@
+#include "entt/entt.hpp"
+
+namespace yumenet {
+
+struct World {
+  entt::registry registry;
+};
+
+} // namespace yumenet
---
+++ src/util/units.hh
@@ -0,0 +1,14 @@
+/**
+ * @file units.hh
+ * @brief Unit-specific aliases for clarity.
+ * 1 spritepx (size of 1 pixel from a sprite) = 2 px
+ * 1 tile (size of a tile in spritepx) = usually 16 spritepx
+ */
+
+namespace yumeami {
+
+using tile_int = int;
+using tile_float = float;
+using spritepx_int = int;
+
+} // namespace yumeami
