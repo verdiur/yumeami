181855d33e42ec0bd34fcf99df011f5ac8d9f632
🌿 <116896120+verdiur@users.noreply.github.com>
2025-08-04 21:45:33 +0000

    feat: add sprinting to input

--- src/game/input.cc
+++ src/game/input.cc
@@ -6,22 +6,32 @@
 #include "util/direction.hh"
 
 void yumeami::input(World &world, entt::dispatcher &dispatcher) {
-  auto view = world.registry.view<PlayerFlag>();
+  auto view = world.registry.view<PlayerFlag, Velocity>();
   if (IsKeyDown(KEY_LEFT)) {
-    for (auto [entity] : view.each()) {
+    for (auto [entity, velocity] : view.each()) {
       dispatcher.trigger(MoveEvent{entity, &world, Direction4::LEFT});
     }
   } else if (IsKeyDown(KEY_RIGHT)) {
-    for (auto [entity] : view.each()) {
+    for (auto [entity, velocity] : view.each()) {
       dispatcher.trigger(MoveEvent{entity, &world, Direction4::RIGHT});
     }
   } else if (IsKeyDown(KEY_UP)) {
-    for (auto [entity] : view.each()) {
+    for (auto [entity, velocity] : view.each()) {
       dispatcher.trigger(MoveEvent{entity, &world, Direction4::TOP});
     }
   } else if (IsKeyDown(KEY_DOWN)) {
-    for (auto [entity] : view.each()) {
+    for (auto [entity, velocity] : view.each()) {
       dispatcher.trigger(MoveEvent{entity, &world, Direction4::BOTTOM});
+    }
+  }
+
+  if (IsKeyPressed(KEY_LEFT_SHIFT)) {
+    for (auto [entity, velocity] : view.each()) {
+      velocity.vel = velocity.vel / 2;
+    }
+  } else if (IsKeyReleased(KEY_LEFT_SHIFT)) {
+    for (auto [entity, velocity] : view.each()) {
+      velocity.vel = velocity.vel * 2;
     }
   }
 }
