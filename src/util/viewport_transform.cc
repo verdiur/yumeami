857fc937b552a243bdca38bf7ba97d1f5b8f0692
🌿 <116896120+verdiur@users.noreply.github.com>
2025-08-05 20:51:17 +0000

    style: fix some styling

--- src/util/viewport_transform.cc
+++ src/util/viewport_transform.cc
@@ -2,7 +2,7 @@
 #include "raylib.h"
 #include <algorithm>
 
-// calc_viewport_scaling
+
 void yumeami::calc_viewport_scaling(const RenderTexture &viewport,
                                     ViewportTransform &viewport_transform) {
   const int vp_width = viewport.texture.width;
@@ -12,6 +12,7 @@ void yumeami::calc_viewport_scaling(const RenderTexture &viewport,
 
   viewport_transform.src_rect = {0, 0, static_cast<float>(vp_width),
                                  -static_cast<float>(vp_height)};
+
   viewport_transform.dst_rect = {
       static_cast<float>(GetScreenWidth() - vp_width * scale_factor) / 2,
       static_cast<float>(GetScreenHeight() - vp_height * scale_factor) / 2,
@@ -19,7 +20,7 @@ void yumeami::calc_viewport_scaling(const RenderTexture &viewport,
       static_cast<float>(vp_height * scale_factor)};
 }
 
-// draw_viewport
+
 void yumeami::draw_viewport(const RenderTexture &viewport,
                             const ViewportTransform &viewport_transform) {
   DrawTexturePro(viewport.texture, viewport_transform.src_rect,
