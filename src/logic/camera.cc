434310c1260efaf06e8e9a2c4051f0b318681348
🌿 <116896120+verdiur@users.noreply.github.com>
2025-09-09 14:52:27 +0000

    feat: add function that calculates camera bounds

--- src/logic/camera.cc
+++ src/logic/camera.cc
@@ -59,3 +59,15 @@ void yumeami::setup_camera(World &world, const SafeRenderTex &vp) {
   };
   update_camera(world, vp, true);
 }
+
+
+yumeami::CameraBounds yumeami::get_camera_bounds(const World &world,
+                                                 const SafeRenderTex &vp) {
+  const Camera2D &cam = world.cam;
+  float left = cam.target.x - cam.offset.x / cam.zoom;
+  float top = cam.target.y - cam.offset.y / cam.zoom;
+  float right = left + vp->texture.width / cam.zoom;
+  float bottom = top + vp->texture.width / cam.zoom;
+
+  return CameraBounds{left, right, top, bottom};
+}
