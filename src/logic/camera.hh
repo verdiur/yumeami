42f6c0587322eedafb166a9b75de3ace7d98c0a0
🌿 <116896120+verdiur@users.noreply.github.com>
2025-09-09 14:52:27 +0000

    feat: add function that calculates camera bounds

--- src/logic/camera.hh
+++ src/logic/camera.hh
@@ -3,6 +3,13 @@
 #include "logic/world.hh"
 namespace yumeami {
 
+  struct CameraBounds {
+    float left;
+    float right;
+    float top;
+    float bottom;
+  };
+
   /**
    * @brief Update camera target. First calculates target, then clamps depending
    * on bounds. If multiple entities are marked as target, then the camera will
@@ -21,5 +28,7 @@ namespace yumeami {
    * @param clamp if true, clamp the target to the boundaries of the world
    */
   void setup_camera(World &world, const SafeRenderTex &vp);
+
+  CameraBounds get_camera_bounds(const World &world, const SafeRenderTex &vp);
 
 } // namespace yumeami
