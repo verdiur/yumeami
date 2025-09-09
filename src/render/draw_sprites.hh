4ebe95145376a712e1fe4de51005ad5b03085144
🌿 <116896120+verdiur@users.noreply.github.com>
2025-09-09 14:53:10 +0000

    refactor: move fallback drawing to their own functions + feat: add off-camera detection function. what the game does with it is   not yet implemented.

--- src/render/draw_sprites.hh
+++ src/render/draw_sprites.hh
@@ -1,26 +1,44 @@
 #pragma once
+#include "common/raii.hh"
 #include "logic/components.hh"
 #include "logic/world.hh"
 #include "resman/texture.hh"
 namespace yumeami {
+
+  enum struct DrawSpriteStatus {
+    OK = 0,
+    NO_SPRITE,
+    NO_SHEET,
+  };
 
   /**
    * @brief Draw all sprite-bearing entities
    * @param world
    * @param pool
    */
-  void draw_sprites(World &world, SheetPool &pool);
+  void draw_sprites(World &world, SafeRenderTex &vp, SheetPool &pool);
 
 } // namespace yumeami
 namespace yumeami::impl {
 
-  Sprite *get_sprite_or_fallback(World &world, entt::entity ent, float dst_x,
-                                 float dst_y);
+  Sprite *get_sprite(World &world, entt::entity ent, float dst_x, float dst_y,
+                     DrawSpriteStatus &status);
 
-  Sheet *get_sheet_or_fallback(World &world, SheetPool &pool,
-                               const Sprite &sprite, float dst_x, float dst_y);
+  Sheet *get_sheet(World &world, SheetPool &pool, const Sprite *sprite,
+                   float dst_x, float dst_y, DrawSpriteStatus &status);
+
+  /**
+   * @brief Tells you if a sprite is off camera. Off camera means that at least
+   * part of the sprite is off camera.
+   *
+   */
+  bool is_sprite_off_camera(const World &world, const SafeRenderTex &vp,
+                            const Sheet *sheet, float dst_x, float dst_y);
 
   void draw_sprite_texture(World &world, const Sheet &sheet,
                            const Sprite &sprite, float dst_x, float dst_y);
+
+  void draw_fallback_no_sprite(World &world, float dst_x, float dst_y);
+  void draw_fallback_no_sheet(World &world, float dst_x, float dst_y);
 
 } // namespace yumeami::impl
