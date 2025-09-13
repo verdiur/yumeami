8417497da47c02bbbfbaf2a5a845dc4bceb4b055
🌿 <116896120+verdiur@users.noreply.github.com>
2025-09-13 11:30:50 +0000

    feat: implement draw_debug_collision

--- src/render/draw_debug.cc
+++ src/render/draw_debug.cc
@@ -8,3 +8,20 @@ void yumeami::draw_debug_info(int text_size) {
   DrawText((std::to_string(fps) + " FPS").c_str(), 3, 3, text_size, BLACK);
   DrawText((std::to_string(fps) + " FPS").c_str(), 2, 2, text_size, GREEN);
 }
+
+
+void yumeami::draw_debug_collision(const World &world) {
+  // NOTE: theres probably a way to optimize this
+  for (int x = 0; x < world.collision.get_width(); x++) {
+    for (int y = 0; y < world.collision.get_height(); y++) {
+      int val = world.collision.at(x, y);
+      if (val > 0) {
+        DrawRectangleLines(x, y, world.tile_size * world.scale,
+                           world.tile_size * world.scale, WHITE);
+        if (val > 1) {
+          DrawText(std::to_string(val).c_str(), x + 2, y + 2, 1, WHITE);
+        }
+      }
+    }
+  }
+}
