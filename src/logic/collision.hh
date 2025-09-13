d183fb3fddf270c72556622f5a093a0751373a90
🌿 <116896120+verdiur@users.noreply.github.com>
2025-09-13 12:22:33 +0000

    feat: add update collision event handling not attached to game loop yet

---
+++ src/logic/collision.hh
@@ -0,0 +1,27 @@
+#pragma once
+#include "entt/signal/fwd.hpp"
+#include "logic/components.hh"
+#include "logic/world.hh"
+namespace yumeami {
+
+  struct UpdateCollisionEvent {
+    World *world;
+    TruePos src;
+    TruePos dst;
+  };
+
+  /**
+   * @brief Connect update collision events to dispatcher
+   * @param dispatcher
+   */
+  void setup_update_collision_event_dispatcher(entt::dispatcher &dispatcher);
+
+  /**
+   * @brief Handle update collision event. Decrements src position and
+   * increments dst position.
+   *
+   * @param event
+   */
+  void handle_update_collision_event(const UpdateCollisionEvent &event);
+
+} // namespace yumeami
