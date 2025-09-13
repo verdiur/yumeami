044598894dc8b41607b4969afcd312c3c6c4c7c3
🌿 <116896120+verdiur@users.noreply.github.com>
2025-09-13 17:14:44 +0000

    feat: add debug draw functions

--- src/render/draw_debug.hh
+++ src/render/draw_debug.hh
@@ -1,9 +1,14 @@
+/**
+ * @file draw_debug.hh
+ * @brief Debug-related draw functions
+ */
+
 #pragma once
 #include "logic/world.hh"
 namespace yumeami {
 
   void draw_debug_info(int text_size);
-
   void draw_debug_collision(const World &world);
+  void draw_debug_world_bounds(const World &world);
 
 } // namespace yumeami
