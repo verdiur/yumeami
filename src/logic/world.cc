4b3d2e7db4b4c27d634df950f0a5f0eb5ec1d138
🌿 <116896120+verdiur@users.noreply.github.com>
2025-09-18 17:31:00 +0000

    style: clean up world.hh and world.cc
    removed unused functions and structs, formatted linebreaks between
    elements properly

--- src/logic/world.cc
+++ src/logic/world.cc
@@ -2,25 +2,6 @@
 #include "logic/components.hh"
 
 
-// yumeami::World yumeami::create_world(tile width, tile height, spx tile_size,
-//                                      bool wrap, bool clamp_camera,
-//                                      std::vector<std::string> sheet_ids,
-//                                      int scale) {
-//   return World{
-//       .width = width,
-//       .height = height,
-//       .tile_size = tile_size,
-//       .wrap = wrap,
-//       .clamp_camera = clamp_camera,
-//       .scale = scale,
-//       .sheet_ids = sheet_ids,
-//       .reg = {},
-//       .collision = Grid<int>(width, height, 0),
-//       .cam = {},
-//   };
-// }
-
-
 yumeami::World yumeami::create_world(const World::Config &config,
                                      const World::Resources &resources) {
   return World{
@@ -39,6 +20,3 @@ void yumeami::setup_world_collision(World &world) {
     world.state.collision.at(true_pos.x, true_pos.y) += 1;
   }
 }
-
-
-void yumeami::unload_world_registry(World &world) { world.state.reg.clear(); }
