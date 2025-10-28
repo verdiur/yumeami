503555837aa9183fca9d37d59875c2b453e30945
🌿 <116896120+verdiur@users.noreply.github.com>
2025-10-28 23:33:02 +0000

    fix: halve viewport resolution

--- src/main.cc
+++ src/main.cc
@@ -6,19 +6,20 @@
 int main(void) {
 
   SetTraceLogLevel(LOG_WARNING);
-  InitWindow(640 * 2, 480 * 2, "yumeami");
+  InitWindow(640, 480, "yumeami");
   InitAudioDevice();
   ChangeDirectory(GetApplicationDirectory());
   SetWindowState(FLAG_VSYNC_HINT);
   SetExitKey(KEY_ESCAPE);
 
   entt::dispatcher dispatcher{};
-  yumeami::Viewport vp(640, 480);
+  yumeami::Viewport vp(320, 240);
   yumeami::update_viewport_texel_scale(vp, dispatcher);
 
   while (!WindowShouldClose()) {
     BeginTextureMode(vp.rt);
     ClearBackground(BLACK);
+    DrawLine(0, 0, 230, 230, WHITE);
     EndTextureMode();
 
     BeginDrawing();
