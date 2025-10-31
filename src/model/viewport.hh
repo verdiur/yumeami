96a6ebd3e7e933c51b1141b2afff5ef6f9763490
🌿 <116896120+verdiur@users.noreply.github.com>
2025-10-31 17:35:47 +0000

    docs: document Viewport

--- src/model/viewport.hh
+++ src/model/viewport.hh
@@ -10,11 +10,26 @@ namespace yumeami {
 
   const entt::hashed_string VIEWPORT_RT_ID = "VIEWPORT";
 
+  /**
+   * @class Viewport
+   * @brief The viewport on the window.
+   *
+   */
   struct Viewport {
-    tx width;
-    tx height;
-    px tx_scale;
-    entt::resource<SafeRenderTexture> rt;
+    tx width;                             // width of the viewport in tx
+    tx height;                            // height of the viewport in tx
+    px tx_scale;                          // how many px is 1 tx?
+    entt::resource<SafeRenderTexture> rt; // render texture. measured in px.
+
+    /**
+     * @brief Viewport constructor. Throws if loading the SafeRenderTexture
+     * fails.
+     *
+     * @param width width of the viewport in tx
+     * @param height height of the viewport in tx
+     * @param tx_scale how many px is 1 tx?
+     * @param rt_cache SafeRenderTexture cache
+     */
     Viewport(tx width, tx height, px tx_scale,
              SafeRenderTextureCache &rt_cache);
   };
