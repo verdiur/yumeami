2155267c14aaa01c782cdd23b473088be94ee733
🌿 <116896120+verdiur@users.noreply.github.com>
2025-10-31 21:24:38 +0000

    style: spacing in raylib_tx.hh

--- src/render/core/raylib_tx.hh
+++ src/render/core/raylib_tx.hh
@@ -5,27 +5,33 @@
 namespace yumeami {
 
   void draw_pixel_tx(px tx_scale, Vector2 position, Color color);
+
   void draw_line_tx(px tx_scale, Vector2 startPos, Vector2 endPos, Color color);
 
   void draw_circle_tx(px tx_scale, Vector2 center, tx radius, Color color);
+
   void draw_circle_lines_tx(px tx_scale, Vector2 center, tx radius,
                             Color color);
 
   void draw_circle_sector_tx(px tx_scale, Vector2 center, tx radius,
                              float startAngle, float endAngle, int segments,
                              Color color);
+
   void draw_circle_sector_lines_tx(px tx_scale, Vector2 center, tx radius,
                                    float startAngle, float endAngle,
                                    int segments, Color color);
 
   void draw_rectangle_tx(px tx_scale, Rectangle rec, Vector2 origin,
                          float rotation, Color color);
+
   void draw_rectangle_lines_tx(px tx_scale, Rectangle rec, Color color);
 
   void draw_texture_tx(px tx_scale, SafeTexture &texture, Vector2 position,
                        Color tint);
+
   void draw_texture_ex_tx(px tx_scale, SafeTexture &texture, Vector2 position,
                           float rotation, float scale, Color tint);
+
   /**
    * @brief Draw a texture with "pro" parameters in "texel mode"
    * @param tx_scale
